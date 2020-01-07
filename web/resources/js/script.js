0a6212e5c186bdb095b028d8f9df5339de7acb29
jenkeyx <megas1200812@ya.ru>
2020-01-07 20:18:20 +0300

    Надписи теперь статичные: меньше кода -- меньше багов! Отрисовка при загрузке

--- web/resources/js/script.js
+++ web/resources/js/script.js
@@ -9,7 +9,7 @@ canvas.height = 400;
 //let radius = 160;
 let width = 400;
 let height = 400;
-let stockRadius = 160;
+let stockRadius = 100;
 c.strokeStyle = "#FFFFFF";
 
 function drawGraph(radius) {
@@ -28,7 +28,9 @@ function drawGraph(radius) {
     c.moveTo(width-10, height/2-5);
     c.lineTo(width-6, height/2);
     c.lineTo(width-10, height/2+5);
+    c.closePath();
     c.stroke();
+
 
 //y-line
     c.beginPath();
@@ -45,28 +47,28 @@ function drawGraph(radius) {
     c.moveTo(width/2-5, 10);
     c.lineTo(width/2, 6);
     c.lineTo(width/2+5, 10);
+    c.closePath();
     c.stroke();
 
 //text
     c.font = "14px Arial";
     c.fillStyle = "#FFFFFF";
     c.fillText("X", width-13, height/2 -5);
-    c.fillText(radius + '/2', width/2 + stockRadius/2, height/2-5);
-    c.fillText(radius, width/2 + stockRadius, height/2-5);
-    c.fillText('-' + radius, width/2 - stockRadius, height/2-5);
-    c.fillText('-' + radius + '/2', width/2 - stockRadius/2, height/2-5);
+    c.fillText('1/2', width/2 + stockRadius/2, height/2-5);
+    c.fillText('1', width/2 + stockRadius, height/2-5);
+    c.fillText('-1', width/2 - stockRadius, height/2-5);
+    c.fillText('-1/2', width/2 - stockRadius/2, height/2-5);
     c.fillText("Y", width/2+5, 13);
-    c.fillText('-' + radius, width/2+5, height/2 + stockRadius);
-    c.fillText('-' + radius + '/2', width/2+5, height/2 + stockRadius/2);
-    c.fillText(radius + '/2', width/2+5, height/2 - stockRadius/2);
+    c.fillText('-1', width/2+5, height/2 + stockRadius);
+    c.fillText('-1/2', width/2+5, height/2 + stockRadius/2);
+    c.fillText('1/2', width/2+5, height/2 - stockRadius/2);
     c.fillText(radius, width/2+5, height/2 - stockRadius);
 
 }
 
 
 function drawFigures(radius) {
-    let shiftRadius = radius*stockRadius/1.5;
-    drawGraph(radius);
+    let shiftRadius = radius*stockRadius;
 // 1part
     c.fillStyle = "rgb(255,255,255,0.4)";
     c.fillRect(width / 2, height / 2 - shiftRadius / 2, shiftRadius, shiftRadius / 2);
@@ -77,6 +79,7 @@ function drawFigures(radius) {
     c.lineTo(width / 2, height / 2);
     c.lineTo(width / 2, height / 2 - shiftRadius);
     c.arc(width / 2, height / 2, shiftRadius, Math.PI, Math.PI * 1.5);
+    c.closePath();
     c.fill();
 
 // 3part triangle 0 r/2 r/2
@@ -85,9 +88,10 @@ function drawFigures(radius) {
     c.lineTo(width / 2 - shiftRadius / 2, height / 2);
     c.lineTo(width / 2, height / 2);
     c.lineTo(width / 2, height / 2 + shiftRadius / 2);
+    c.closePath();
     c.fill();
+    drawGraph(radius);
 }
-
 function drawDot(x, y, hit) {
     c.fillStyle = hit ? "#009900" : "#990000";
     c.beginPath();
@@ -107,14 +111,6 @@ function drawAllDots() {
         c.fill();
     }
 }
-
-function draw() {
-    drawGraph(stockRadius);
-    drawFigures(stockRadius);
-    drawAllDots();
-}
-
-
 canvas.addEventListener("mousedown", function (event) {
     let click_x, click_y;
     let r = document.getElementById("rValue");
@@ -134,3 +130,4 @@ function sendCanvas(x, y) {
     let button = document.getElementById("submitButton1");
     button.click();
 }
+
