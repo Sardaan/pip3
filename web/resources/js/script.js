95b0b901ef842929cbdd6849a5d3d1b4c7cdea15
jenkeyx <megas1200812@ya.ru>
2020-01-12 14:51:24 +0300

    Теперь канвас при изменении области изменяет цвет точек в зависимости от их положения

--- web/resources/js/script.js
+++ web/resources/js/script.js
@@ -6,69 +6,69 @@ let canvas = document.querySelector("#shapesCanvas");
 let c = canvas.getContext("2d");
 canvas.width = 400;
 canvas.height = 400;
-//let radius = 160;
 let width = 400;
 let height = 400;
 let stockRadius = 60;
 c.strokeStyle = "#FFFFFF";
 
-function drawGraph(radius) {
+
+function drawGraph() {
 // x-line
     c.beginPath();
-    c.moveTo(5, height/2);
-    c.lineTo(width-5, height/2);
-    c.moveTo(width/2-stockRadius, height/2-3);
-    c.lineTo(width/2-stockRadius, height/2+3);
-    c.moveTo(width/2-stockRadius*2, height/2-3);
-    c.lineTo(width/2-stockRadius*2, height/2+3);
-    c.moveTo(width/2+stockRadius*2, height/2-3);
-    c.lineTo(width/2+stockRadius*2, height/2+3);
-    c.moveTo(width/2+stockRadius, height/2-3);
-    c.lineTo(width/2+stockRadius, height/2+3);
-    c.moveTo(width-10, height/2-5);
-    c.lineTo(width-6, height/2);
-    c.lineTo(width-10, height/2+5);
+    c.moveTo(5, height / 2);
+    c.lineTo(width - 5, height / 2);
+    c.moveTo(width / 2 - stockRadius, height / 2 - 3);
+    c.lineTo(width / 2 - stockRadius, height / 2 + 3);
+    c.moveTo(width / 2 - stockRadius * 2, height / 2 - 3);
+    c.lineTo(width / 2 - stockRadius * 2, height / 2 + 3);
+    c.moveTo(width / 2 + stockRadius * 2, height / 2 - 3);
+    c.lineTo(width / 2 + stockRadius * 2, height / 2 + 3);
+    c.moveTo(width / 2 + stockRadius, height / 2 - 3);
+    c.lineTo(width / 2 + stockRadius, height / 2 + 3);
+    c.moveTo(width - 10, height / 2 - 5);
+    c.lineTo(width - 6, height / 2);
+    c.lineTo(width - 10, height / 2 + 5);
     c.closePath();
     c.stroke();
 
 
 //y-line
     c.beginPath();
-    c.moveTo(width/2, 5);
-    c.lineTo(width/2, height-5);
-    c.moveTo(width/2-3, height/2-stockRadius);
-    c.lineTo(width/2+3, height/2-stockRadius);
-    c.moveTo(width/2-3, height/2-stockRadius*2);
-    c.lineTo(width/2+3, height/2-stockRadius*2);
-    c.moveTo(width/2-3, height/2+stockRadius*2);
-    c.lineTo(width/2+3, height/2+stockRadius*2);
-    c.moveTo(width/2-3, height/2+stockRadius);
-    c.lineTo(width/2+3, height/2+stockRadius);
-    c.moveTo(width/2-5, 10);
-    c.lineTo(width/2, 6);
-    c.lineTo(width/2+5, 10);
+    c.moveTo(width / 2, 5);
+    c.lineTo(width / 2, height - 5);
+    c.moveTo(width / 2 - 3, height / 2 - stockRadius);
+    c.lineTo(width / 2 + 3, height / 2 - stockRadius);
+    c.moveTo(width / 2 - 3, height / 2 - stockRadius * 2);
+    c.lineTo(width / 2 + 3, height / 2 - stockRadius * 2);
+    c.moveTo(width / 2 - 3, height / 2 + stockRadius * 2);
+    c.lineTo(width / 2 + 3, height / 2 + stockRadius * 2);
+    c.moveTo(width / 2 - 3, height / 2 + stockRadius);
+    c.lineTo(width / 2 + 3, height / 2 + stockRadius);
+    c.moveTo(width / 2 - 5, 10);
+    c.lineTo(width / 2, 6);
+    c.lineTo(width / 2 + 5, 10);
     c.closePath();
     c.stroke();
 
 //text
     c.font = "14px Arial";
     c.fillStyle = "#FFFFFF";
-    c.fillText("X", width-13, height/2 -5);
-    c.fillText('2', width/2 + stockRadius*2, height/2-5);
-    c.fillText('1', width/2 + stockRadius, height/2-5);
-    c.fillText('-1', width/2 - stockRadius, height/2-5);
-    c.fillText('-2', width/2 - stockRadius*2, height/2-5);
-    c.fillText("Y", width/2+5, 13);
-    c.fillText('-1', width/2+5, height/2 + stockRadius);
-    c.fillText('-2', width/2+5, height/2 + stockRadius*2);
-    c.fillText('2', width/2+5, height/2 - stockRadius*2);
-    c.fillText("1", width/2+5, height/2 - stockRadius);
+    c.fillText("X", width - 13, height / 2 - 5);
+    c.fillText('2', width / 2 + stockRadius * 2, height / 2 - 5);
+    c.fillText('1', width / 2 + stockRadius, height / 2 - 5);
+    c.fillText('-1', width / 2 - stockRadius, height / 2 - 5);
+    c.fillText('-2', width / 2 - stockRadius * 2, height / 2 - 5);
+    c.fillText("Y", width / 2 + 5, 13);
+    c.fillText('-1', width / 2 + 5, height / 2 + stockRadius);
+    c.fillText('-2', width / 2 + 5, height / 2 + stockRadius * 2);
+    c.fillText('2', width / 2 + 5, height / 2 - stockRadius * 2);
+    c.fillText("1", width / 2 + 5, height / 2 - stockRadius);
 
 }
 
 
 function drawFigures(radius) {
-    let shiftRadius = radius*stockRadius;
+    let shiftRadius = radius * stockRadius;
 // 1part
     c.fillStyle = "rgb(255,255,255,0.4)";
     c.fillRect(width / 2, height / 2 - shiftRadius / 2, shiftRadius, shiftRadius / 2);
@@ -92,6 +92,7 @@ function drawFigures(radius) {
     c.fill();
     drawGraph(radius);
 }
+
 function drawDot(x, y, hit) {
     c.fillStyle = hit ? "#009900" : "#990000";
     c.beginPath();
@@ -100,26 +101,29 @@ function drawDot(x, y, hit) {
 
 }
 
-function drawAllDots(){
+function drawAllDots(r) {
     let dots = document.getElementById("data").querySelectorAll("*");
     for (let i = 0; i < dots.length; i++) {
         let x = dots[i].getAttribute("data-x");
         let y = dots[i].getAttribute("data-y");
-        let r = dots[i].getAttribute("data-r");
-        let hit = checkArea(x,y,r);
-        drawDot(x*stockRadius+200, 200-y*stockRadius, hit);
+        if (i === 1){
+            console.log(x,y,r);
+        }
+        let hit = checkArea(x, y, r);
+        drawDot(x * stockRadius + 200, 200 - y * stockRadius, hit);
     }
 }
 
 //при клике обновляется все страница (мб надо чтобы это был аякс клик?) еще почему то все время последнее значение пересылается в бд значения
 canvas.addEventListener("mousedown", function (event) {
     let click_x, click_y;
-    let r = PF("rSelect").getSelectedValue();
+
     let rect = canvas.getBoundingClientRect();
     click_x = event.clientX - rect.left;
     click_y = event.clientY - rect.top;
     let x = (click_x - 200) / stockRadius;
     let y = (-click_y + 200) / stockRadius;
+    let r = PF("rSelect").getSelectedValue();
     drawDot(click_x, click_y, checkArea(x.toFixed(3), y.toFixed(3), r));
 
     document.getElementById("canvasForm:xHidden").value = x.toFixed(3);
@@ -131,14 +135,11 @@ canvas.addEventListener("mousedown", function (event) {
 
 
 function checkArea(xVal, yVal, rVal) {
-    if(xVal>=0 && yVal>=0 && xVal<=rVal && yVal<=rVal/2) return true;
-    if(xVal<=0 && yVal>=0 && rVal*rVal >= xVal*xVal+yVal*yVal)return true;
-    if(xVal<=0 && yVal<=0 && yVal >= -xVal-rVal/2) return true;
-    return false;
+    return (xVal >= 0 && yVal >= 0 && xVal <= rVal && yVal <= rVal / 2) || (xVal <= 0 && yVal <= 0 && yVal >= -xVal - rVal / 2) || (xVal <= 0 && yVal >= 0 && rVal * rVal >= xVal * xVal + yVal * yVal);
 }
+
 function drawCanvas(radius) {
-    c.clearRect(0,0,width,height);
-    drawAllDots();
+    c.clearRect(0, 0, width, height);
     drawFigures(radius);
-    drawAllDots(radius)
+    drawAllDots(radius);
 }
